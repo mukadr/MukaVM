444f2cb4a9415423074ab1f5ff55d0673ba1114a
Mauro Dreissig <mukadr@gmail.com>
2021-05-16 17:05:27 -0300

    CfgTest: Refactor

--- src/MukaVM.Test/Graph/CfgTest.cs
+++ src/MukaVM.Test/Graph/CfgTest.cs
@@ -7,27 +7,26 @@ namespace MukaVM.Test.Graph
     public class CfgTest
     {
         [Fact]
-        public void Test1()
+        public void GeneratesMoreThanOneBasicBlockForFunctionWithLabels()
         {
             var x = new Var("x");
-            var greaterThanZero = new Label("greaterThanZero");
+            var gt0 = new Label("gt0");
             var exit = new Label("exit");
-
-            var f1 = new Function(
-                "f1",
+            var simple1 = new Function(
+                "simple1",
                 new List<Instruction>
                 {
                     new Add(x, new Int(0), new Int(1)),
-                    new Jg(x, new Int(0), greaterThanZero),
+                    new Jg(x, new Int(0), gt0),
                     new Add(x, x, new Int(2)),
                     new Jmp(exit),
-                    greaterThanZero,
+                    gt0,
                     new Add(x, x, new Int(5)),
                     exit,
                     new Ret()
                 });
 
-            var cfg = IR.Graph.Convert.ToControlFlowGraph(f1);
+            var cfg = IR.Graph.Convert.ToControlFlowGraph(simple1);
 
             var bb1 = new IR.Graph.BasicBlock("BB1");
             var bb2 = new IR.Graph.BasicBlock("BB2");
@@ -36,35 +35,23 @@ namespace MukaVM.Test.Graph
             
             var bb3Label = new IR.Graph.Label(bb3);
             var bb4Label = new IR.Graph.Label(bb4);
-            
-            bb1.Instructions =
-                new List<Instruction>
-                {
-                    new Add(x, new Int(0), new Int(1)),
-                    new Jg(x, new Int(0), bb3Label)
-                };
 
-            bb2.Instructions =
-                new List<Instruction>
-                {
-                    new Add(x, x, new Int(2)),
-                    new Jmp(bb4Label)
-                };
+            // BB1
+            bb1.Instructions.Add(new Add(x, new Int(0), new Int(1)));
+            bb1.Instructions.Add(new Jg(x, new Int(0), bb3Label));
 
-            bb3.Instructions =
-                new List<Instruction>
-                {
-                    new Add(x, x, new Int(5))
-                };
+            // BB2
+            bb2.Instructions.Add(new Add(x, x, new Int(2)));
+            bb2.Instructions.Add(new Jmp(bb4Label));
 
-            bb4.Instructions =
-                new List<Instruction>
-                {
-                    new Ret()
-                };
+            // BB3
+            bb3.Instructions.Add(new Add(x, x, new Int(5)));
+
+            // BB4
+            bb4.Instructions.Add(new Ret());
 
             var expectedCfg = new IR.Graph.Function(
-                f1.Name,
+                simple1.Name,
                 new List<IR.Graph.BasicBlock>
                 {
                     bb1,
