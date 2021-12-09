b3cc3385b6abdf4911a502aa518c55efde98b1ae
Mauro Dreissig <mukadr@gmail.com>
2021-12-09 10:32:36 -0300

    Test: CFG: Validate loop

--- src/MukaVM.Test/IR/ControlFlowGraphTest.cs
+++ src/MukaVM.Test/IR/ControlFlowGraphTest.cs
@@ -82,5 +82,45 @@ namespace MukaVM.Test.IR
 
             Util.AssertControlFlowGraphEquals(expected, Parse.FromSourceText(sourceText));
         }
+
+        [Fact]
+        public void FunctionWithLoops_Is_Handled_Correctly()
+        {
+            const string sourceText = @"
+                FUNCTION f {
+                    x = 0 + 1
+                    again
+                    x = x + 1
+                    IF x > 10: end
+                    JMP again
+                    end
+                    RET
+                }";
+
+            const string expected = @"
+                FUNCTION f {
+                    BB1 {
+                        x = 0 + 1
+                        <BB2>
+                    }
+                    BB2 {
+                        <BB1, BB3>
+                        x = x + 1
+                        IF x > 10: BB4
+                        <BB3, BB4>
+                    }
+                    BB3 {
+                        <BB2>
+                        JMP BB2
+                        <BB2>
+                    }
+                    BB4 {
+                        <BB2>
+                        RET
+                    }
+                }";
+
+            Util.AssertControlFlowGraphEquals(expected, Parse.FromSourceText(sourceText));
+        }
     }
 }
