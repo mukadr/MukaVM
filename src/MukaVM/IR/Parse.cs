f3e9f34945b52a465981eaee21d7bd5a57b0f19a
Mauro Dreissig <mukadr@gmail.com>
2021-12-11 11:32:46 -0300

    Parse: Refactor and locate existing variables

--- src/MukaVM/IR/Parse.cs
+++ src/MukaVM/IR/Parse.cs
@@ -16,6 +16,67 @@ namespace MukaVM.IR
 
         public static Function FromSourceText(string sourceText)
         {
+            var variables = new List<Var>();
+
+            Var FindVariable(string name)
+            {
+                var @var = variables.SingleOrDefault(v => v.Name == name);
+                if (@var is null)
+                {
+                    throw new ParserException($"Undeclared variable {name}.");
+                }
+                return @var;
+            }
+
+            Var FindOrCreateVariable(string name)
+            {
+                var @var = variables.SingleOrDefault(v => v.Name == name);
+                if (@var is null)
+                {
+                    @var = new Var(name);
+                    variables.Add(@var);
+                }
+                return @var;
+            }
+
+            var labels = new List<Label>();
+
+            Label CreateLabel(string labelName)
+            {
+                var label = new Label(labelName);
+                if (labels.Any(l => l.Name == label.Name))
+                {
+                    throw new ParserException($"Label {label} already declared.");
+                }
+                labels.Add(label);
+                return label;
+            }
+
+            var jmpTargets = new List<JmpTarget>();
+
+            JmpTarget FindOrCreateJmpTarget(string labelName)
+            {
+                var target = jmpTargets.SingleOrDefault(t => t.Name == labelName);
+                if (target is null)
+                {
+                    target = new JmpTarget(labelName);
+                    jmpTargets.Add(target);
+                }
+                return target;
+            }
+
+            List<Instruction> UpdateJmpTargets(List<Instruction> instructions)
+            {
+                foreach (var instruction in instructions)
+                {
+                    if (instruction is Jmp jmp)
+                    {
+                        jmp.Target = labels.Single(l => l.Name == jmp.Target.Name);
+                    }
+                }
+                return instructions;
+            }
+
             var letter = Match('a', 'z').Or(Match('A', 'Z'));
             var digit = Match('0', '9');
             var integer = Token(OneOrMore(digit));
@@ -31,65 +92,29 @@ namespace MukaVM.IR
             var openBrace = Token("{");
             var closeBrace = Token("}");
 
-            var variables = new List<Var>();
-            var labels = new List<Label>();
-            var jmpTargets = new List<JmpTarget>();
-
             var argument =
                 integer.Map<Value>(i => new Int(i.Value))
-                .Or(identifier.Map<Value>(id => new Var(id.Value)));
+                .Or(identifier.Map<Value>(id => FindVariable(id.Value)));
 
             var addInstruction =
                 identifier.Bind(id =>
                     assign.And(argument).Bind(left =>
                         plus.And(argument).Map<Instruction>(right =>
-                        {
-                            var v = variables.SingleOrDefault(v => v.Name == id.Value);
-                            if (v is null)
-                            {
-                                v = new Var(id.Value);
-                                variables.Add(v);
-                            }
-                            return new Add(v, left, right);
-                        })));
+                            new Add(FindOrCreateVariable(id.Value), left, right))));
 
             var labelInstruction =
-                identifier.Map<Instruction>(id =>
-                {
-                    var l = new Label(id.Value);
-                    if (labels.Any(ll => ll.Name == l.Name))
-                    {
-                        throw new ParserException($"Label {l} already declared.");
-                    }
-                    labels.Add(l);
-                    return l;
-                });
+                identifier.Map<Instruction>(labelName =>
+                    CreateLabel(labelName.Value));
 
             var jmpInstruction =
-                jmpKw.And(identifier.Map<Instruction>(id =>
-                {
-                    var target = jmpTargets.SingleOrDefault(t => t.Name == id.Value);
-                    if (target is null)
-                    {
-                        target = new JmpTarget(id.Value);
-                        jmpTargets.Add(target);
-                    }
-                    return new Jmp(target);
-                }));
+                jmpKw.And(identifier.Map<Instruction>(labelName =>
+                    new Jmp(FindOrCreateJmpTarget(labelName.Value))));
 
             var ifInstruction =
                 ifKw.And(argument.Bind(left =>
                     gt.And(argument.Bind(right =>
                         colon.And(identifier.Map<Instruction>(labelName =>
-                        {
-                            var target = jmpTargets.SingleOrDefault(t => t.Name == labelName.Value);
-                            if (target is null)
-                            {
-                                target = new JmpTarget(labelName.Value);
-                                jmpTargets.Add(target);
-                            }
-                            return new Jg(left, right, target);
-                        }))))));
+                            new Jg(left, right, FindOrCreateJmpTarget(labelName.Value))))))));
 
             var retInstruction =
                 retKw.Map<Instruction>(_ => new Ret());
@@ -106,16 +131,7 @@ namespace MukaVM.IR
                 .And(functionKw.And(identifier.Bind(name =>
                     openBrace.And(ZeroOrMore(instruction).Bind(instructions =>
                         closeBrace.Map(_ =>
-                        {
-                            foreach (var instruction in instructions)
-                            {
-                                if (instruction is Jmp jmp)
-                                {
-                                    jmp.Target = labels.Single(l => l.Name == jmp.Target.Name);
-                                }
-                            }
-                            return new Function(name.Value, instructions);
-                        }))))));
+                            new Function(name.Value, UpdateJmpTargets(instructions))))))));
 
             return functionDefinition.ParseAllText(sourceText);
         }
