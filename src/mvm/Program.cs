64dcffb1f0cb8d6e826c4671571949e24588a086
Mauro Dreissig <mukadr@gmail.com>
2021-12-14 09:03:42 -0300

    Add main program

---
+++ src/mvm/Program.cs
@@ -0,0 +1,14 @@
+﻿using MukaVM.IR;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Usage: mvm <source-file>");
+    return;
+}
+
+var sourceText = File.ReadAllText(args[0]);
+
+var cfg = CFG.Convert(Parse.FromSourceText(sourceText));
+SSA.Transform(cfg);
+
+Console.WriteLine(cfg);
