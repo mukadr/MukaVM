eea5c528d5c51f65820fb316251e2befd97c7c42
Mauro Dreissig <mukadr@gmail.com>
2024-05-13 18:08:56 -0300

    IR: Refactor Jmp to avoid code duplication

--- MukaVM/IR/Instructions/Jmp.cs
+++ MukaVM/IR/Instructions/Jmp.cs
@@ -8,34 +8,36 @@ public abstract class JmpInstruction(Label target, params Value[] operands) : In
 public class Jmp(Label target, params Value[] values) : JmpInstruction(target, values)
 {
     public override string ToString() => "JMP " + Target;
+
+    protected string ToConditionalJmpString(string op) => "IF " + Operands[0] + $" {op} " + Operands[1] + ": " + Target;
 }
 
 public class Je(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " = " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString("=");
 }
 
 public class Jne(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " != " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString("!=");
 }
 
 public class Jg(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " > " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString(">");
 }
 
 public class Jl(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " < " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString("<");
 }
 
 public class Jge(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " >= " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString(">=");
 }
 
 public class Jle(Value value1, Value value2, Label target) : Jmp(target, value1, value2)
 {
-    public override string ToString() => "IF " + Operands[0] + " <= " + Operands[1] + ": " + Target;
+    public override string ToString() => ToConditionalJmpString("<=");
 }
